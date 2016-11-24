6b40323ebccd95a36e8a178dd112cf9feda64425
Kiril_Velichkov <kvelichkow@gmail.com>
2016-11-24 11:37:42 +0200

    adding more stuff

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require('express');
 
 let app = express();
 
+let stage = 'development';
+
+let config = require('./server/config/config')[stage];
+
+app.use(express.static(config.rootPath + 'public'));
+app.set('view engine', 'pug');
+app.set('views', config.rootPath + 'server/views');
+
+app.get('/', (req, res) => {
+    res.send('here');
+});
+
+
+
+app.listen(config.port, () => console.log('Server running at port : ' + config.port));
+
+
