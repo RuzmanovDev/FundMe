58844568a58fd92dd98e602b8a67ff58a1fec504
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-11-24 12:18:24 +0200

    Added bootstrap

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ let stage = 'development';
 
 let config = require('./server/config/config')[stage];
 
-app.use(express.static(config.rootPath + 'public'));
 app.set('view engine', 'pug');
+app.use('/public', express.static(config.rootPath + '/public'));
+app.use('/bower_components', express.static(config.rootPath + '/bower_components'));
 app.set('views', config.rootPath + 'server/views');
 
 app.get('/', (req, res) => {
-    res.send('here');
+    res.render('home/home');
 });
 
 
