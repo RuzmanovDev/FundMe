e184c9ce5ace8f1bc6bb40c626db183952d57416
Kiril_Velichkov <kvelichkow@gmail.com>
2016-11-24 12:26:58 +0200

    fucking typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 /// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-=======
 /// <reference path="globals/express-serve-static-core/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/mongoose/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
->>>>>>> 78a5d7f43ffc5bebb13f9b93a01d1e485dbba658
