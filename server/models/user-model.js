8b0e1b1fe7648789df8f8a00f760fbefb4665f66
sguzunov <sguzunov@gmail.com>
2016-11-25 01:11:15 +0200

    Changed user model fields to be required. Added email regex validation.

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,49 +1,63 @@
-/* globals const, module, require */
+/* globals module, require */
+
 'use strict';
 
 const mongoose = require('mongoose');
 
-const minUsernameLength = 3;
-const maxUsernameLength = 20;
+const fieldsValidator = require('./validator');
 
-function validateLength(value, min, max) {
-    if (!max) {
-        max = min;
-        min = 3;
-    }
-
-    return min <= value.length && value.length <= max;
-}
+const MinUsernameLength = 3;
+const MaxUsernameLength = 20;
+const MinPasswordLength = 3;
+const MaxPasswordLength = 20;
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         required: true,
         unique: true,
         validate: {
-            validator: function (value) {
-                return validateLength(value, minUsernameLength, maxUsernameLength);
+            validator: function(value) {
+                return fieldsValidator.validateLength(value, MinUsernameLength, MaxUsernameLength);
             },
             message: '{VALUE} is not a valid username!'
         }
     },
     passHash: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(value) {
+                return fieldsValidator.validateLength(value, MinPasswordLength, MaxPasswordLength);
+            },
+            message: '{VALUE} is not a valid pass hash!'
+        }
     },
     email: {
         type: String,
         unique: true,
         validate: {
-            validator: function (value) {
-                // TO DO : REGEX for email
+            validator: function(value) {
+
+                // http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+                // var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                // return re.test(value);
             },
-            message: '{VALUE} is not a valid pass!'
+            message: '{VALUE} is not a valid email!'
         }
     },
-    salt: String,
+    salt: {
+        type: String,
+        required: true
+    },
     roles: [String]
 });
 
