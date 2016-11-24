267093bdc2e9c64455a510ff4c2fde59c19f3d98
gyankov <g.yankov951@abv.bg>
2016-11-24 15:19:46 +0200

    Campaign by id

--- server/data/campaign-data.js
+++ server/data/campaign-data.js
@@ -4,14 +4,25 @@ module.exports = function (models) {
     let Campaign = models.Campaign;
 
     return {
-        GetAllCampaigns() {
+        getAllCampaigns() {
             return new Promise((resolve, reject) => {
-                Campaign.find((err, campaigns)=>{
+                Campaign.find((err, campaigns) => {
                     if (err) {
                         return reject(err);
                     }
 
                     return resolve(campaigns);
+                });
+            });
+        },
+        getCampaignById(id) {
+            return new Promise((resolve, reject) => {
+                Campaign.findOne({ _id: id }, (err, campaign) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    return resolve(campaign);
                 });
             });
         }
