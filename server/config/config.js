b7848acb7706c8e027b50291c5b36a1e182956da
Kiril_Velichkov <kvelichkow@gmail.com>
2016-11-26 15:51:36 +0200

    conn string update adding auth.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ let rootPath = path.normalize(path.join(__dirname, '/../../'));
 module.exports = {
     development: {
         rootPath: rootPath,
-        localDB: 'mongodb://localhost:27017/testing2',
+        localDB: 'mongodb://localhost:27017/testing',
         cloudDB: 'mongodb://admin:djagascript1234@ds161487.mlab.com:61487/djagascript',
         localPort: 1337,
         cloudPort: 8080
