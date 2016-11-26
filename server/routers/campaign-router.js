e79fcf4c83ab1e1f7782824225969211c757e6cb
gyankov <g.yankov951@abv.bg>
2016-11-26 14:32:52 +0200

    Create campaign

--- server/routers/campaign-router.js
+++ server/routers/campaign-router.js
@@ -6,8 +6,9 @@ module.exports = function (app, data){
     let router = new express.Router();
 
     router
-    .get('/', controller.getAll)
-    .get('/:id', controller.getById);
+    .get('/', controller.getAll)    
+    .get('/create', controller.getCreateForm)
+    .get('/campaign/:id', controller.getById);
 
     app.use('/campaigns', router);
 };
