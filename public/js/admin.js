ebd282e3611ebdeb01c97705e18f60abbbbfbf68
sguzunov <sguzunov@gmail.com>
2016-11-29 22:24:08 +0200

    Admin panel for users will use a grid.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,58 @@
+/* globals $ */
 'use strict';
+
+$(function() {
+
+    $("#all-users").jsGrid({
+        height: "70%",
+        width: "100%",
+        filtering: true,
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+        autoload: true,
+        pageSize: 10,
+        pageButtonCount: 5,
+        deleteConfirm: "Do you really want to delete a user?",
+        controller: {
+            loadData: function(filter) {
+                return $.ajax({
+                    type: "GET",
+                    url: "/clients",
+                    data: filter
+                });
+            },
+            insertItem: function(item) {
+                return $.ajax({
+                    type: "POST",
+                    url: "/clients",
+                    data: item
+                });
+            },
+            updateItem: function(item) {
+                return $.ajax({
+                    type: "PUT",
+                    url: "/clients",
+                    data: item
+                });
+            },
+            deleteItem: function(item) {
+                return $.ajax({
+                    type: "DELETE",
+                    url: "/clients",
+                    data: item
+                });
+            }
+        },
+        fields: [
+            { name: "Name", type: "text", width: 150 },
+            { name: "Age", type: "number", width: 50, filtering: false },
+            { name: "Address", type: "text", width: 200 },
+            { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+            { type: "control" }
+        ]
+    });
+
+});
