4af903d38d7863c9ce10ac74478f51d3ae0f79c1
sguzunov <sguzunov@gmail.com>
2016-11-30 00:36:25 +0200

    Admin users grid set up.

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,9 +3,9 @@
 
 $(function() {
 
-    $("#all-users").jsGrid({
-        height: "70%",
-        width: "100%",
+    $('#all-users').jsGrid({
+        height: '700px',
+        width: '100%',
         filtering: true,
         inserting: true,
         editing: true,
@@ -13,45 +13,45 @@ $(function() {
         paging: true,
         autoload: true,
         pageSize: 10,
-        pageButtonCount: 5,
-        deleteConfirm: "Do you really want to delete a user?",
+        pageButtonCount: 3,
+        deleteConfirm: 'Do you really want to delete a user?',
         controller: {
             loadData: function(filter) {
                 return $.ajax({
-                    type: "GET",
-                    url: "/clients",
+                    type: 'GET',
+                    url: '/admin/users',
                     data: filter
                 });
             },
             insertItem: function(item) {
                 return $.ajax({
-                    type: "POST",
-                    url: "/clients",
+                    type: 'POST',
+                    url: '/admin/users',
                     data: item
                 });
             },
             updateItem: function(item) {
                 return $.ajax({
-                    type: "PUT",
-                    url: "/clients",
+                    type: 'PUT',
+                    url: '/admin/users',
                     data: item
                 });
             },
             deleteItem: function(item) {
                 return $.ajax({
-                    type: "DELETE",
-                    url: "/clients",
+                    type: 'DELETE',
+                    url: '/admin/users',
                     data: item
                 });
             }
         },
         fields: [
-            { name: "Name", type: "text", width: 150 },
-            { name: "Age", type: "number", width: 50, filtering: false },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-            { type: "control" }
+            { name: 'username', type: 'text', title: 'Username', width: '15%', filtering: true, sorting: true },
+            { name: 'email', type: 'text', title: 'E-mail', width: '15%', filtering: true, sorting: true },
+            { name: 'firstname', type: 'text', title: 'First Name', width: '15%', filtering: true, sorting: true },
+            { name: 'lastname', type: 'text', title: 'Last Name', width: '15%', filtering: true, sorting: true },
+            { name: 'isAdmin', type: 'checkbox', title: 'Is Admin', filtering: true },
+            { type: 'control' }
         ]
     });
 
