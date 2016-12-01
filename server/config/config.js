39f328d03addc34ee68aec3c0f1b94c00828ca3f
Kiril_Velichkov <kvelichkow@gmail.com>
2016-12-01 12:50:25 +0200

    adjusted config for production enviroment

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
 let rootPath = path.normalize(path.join(__dirname, '/../../'));
-
+const PORT = process.env.PORT || 1337;
 module.exports = {
     development: {
         rootPath: rootPath,
-        localDB: 'mongodb://localhost:27017/testing',
-        cloudDB: 'mongodb://admin:djagascript1234@ds161487.mlab.com:61487/djagascript',
-        localPort: 1337,
-        cloudPort: 8080
+        connectionString: 'mongodb://localhost:27017/testing',
+        port: PORT
+    },
+    production: {
+        rootPath: rootPath,
+        connectionString: 'mongodb://admin:djagascript1234@ds161487.mlab.com:61487/djagascript',
+        port: PORT
     }
 };
