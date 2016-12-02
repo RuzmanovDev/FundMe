bd6934f7bc25657dd24e51fbd919838986c42f26
Kiril_Velichkov <kvelichkow@gmail.com>
2016-12-02 19:08:43 +0200

    all invalid routes are redirected to the error page

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,11 +4,15 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function({ app, data, database, upload, auth, grid, encryption, userMiddleware }) {
+module.exports = function ({ app, data, database, upload, auth, grid, encryption, userMiddleware }) {
 
     fs.readdirSync(__dirname)
         .filter(x => x.includes('-router'))
         .forEach(file => {
             require(path.join(__dirname, file))({ app, data, database, upload, auth, grid, encryption, userMiddleware });
         });
+
+    app.get('*', (req, res) => {
+        res.redirect('home/error');
+    });
 };
