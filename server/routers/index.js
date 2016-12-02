80aa30f9242ad6fa59a21c8bf9430142ba87de96
sguzunov <sguzunov@gmail.com>
2016-12-02 18:23:18 +0200

    All routes use middleware for logged user.

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,11 +4,11 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function ({ app, data, database, upload, auth, grid, encryption }) {
+module.exports = function({ app, data, database, upload, auth, grid, encryption, userMiddleware }) {
 
     fs.readdirSync(__dirname)
         .filter(x => x.includes('-router'))
         .forEach(file => {
-            require(path.join(__dirname, file))({ app, data, database, upload, auth, grid, encryption });
+            require(path.join(__dirname, file))({ app, data, database, upload, auth, grid, encryption, userMiddleware });
         });
 };
--- server/routers/messages-router.js
+++ server/routers/messages-router.js
@@ -1,12 +1,13 @@
 const auth = require('../config/auth');
 const router = require('express').Router();
 
-module.exports = function (options) {
+module.exports = function(options) {
     let controller = require('../controllers/messages-controller')(options);
 
     router
-    .get('/', auth.isAuthenticated, controller.getMessageForm)
-    //.get('/:id',auth.isAuthenticated,controller.exactMessage);
+        .get('/', auth.isAuthenticated, controller.getMessageForm)
+        //.get('/:id',auth.isAuthenticated,controller.exactMessage);
 
+    options.app.use(options.userMiddleware.hasLoggedUser);
     options.app.use('/messages', router);
 };
