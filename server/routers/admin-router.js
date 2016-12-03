d8282f9803894bda68c791f9ab9b3a8cf2ee9b58
sguzunov <sguzunov@gmail.com>
2016-12-03 16:18:14 +0200

    Fixes....

--- server/routers/admin-router.js
+++ server/routers/admin-router.js
@@ -6,7 +6,7 @@ module.exports = function({ data, app, auth, userMiddleware }) {
     const adminController = require('../controllers/admin-controller')({ data });
 
     router
-        .get('/profiles', /*auth.isAuthenticated, auth.isInRole('admin'), */ adminController.getProfiles)
+        .get('/profiles', /*auth.isAuthenticated,*/ /*auth.isInRole('admin'),*/ adminController.getProfiles)
         .put('/users', /* auth.isAuthenticated, auth.isInRole('admin'), */ adminController.updateUser)
         .get('/users', /*auth.isAuthenticated, auth.isInRole('Admin'), */ adminController.getUsersData);
 
