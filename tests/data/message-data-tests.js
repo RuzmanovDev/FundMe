0ce873cc414f16fd7d6d1eb32818ec8549692102
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-12-03 17:08:19 +0200

    added skeleton for message-data unit tests

---
+++ tests/data/message-data-tests.js
@@ -0,0 +1,48 @@
+/* globals it describe beforeEach afterEach */
+
+const chai = require('chai');
+const sinonModule = require('sinon');
+
+let {expect} = chai;
+
+describe('Message data test', () => {
+    let sinon;
+    let Message = require('./mocks/message-data-mocks');
+    let data = require('../../server/data/message-data')({ Message });
+
+    beforeEach(() => {
+        sinon = sinonModule.sandbox.create();
+    });
+
+    describe('loadCurrentConversations()', () => {
+        let firstUserId = 2;
+
+        let firstUser = {
+            _id: firstUserId
+        };
+
+        let expectedMessages = [{
+            firstUser,
+            content: 'test'
+        }];
+
+        beforeEach(() => {
+            sinon.stub(Message, 'find', (query, cb) => {
+                let id = query._id;
+                cb(null, expectedMessages);
+            });
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('Expect to return messages in conversation by given firstUserID', (done) => {
+            data.findByIdentification(firstUserId)
+                .then((messages) => {
+                    expect(messages).to.equal(expectedMessages);
+                })
+                .then(done, done);
+        });
+    });
+});
